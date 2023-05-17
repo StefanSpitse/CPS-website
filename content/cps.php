86a2809cc93c44cc7ce0dfe9a1b77b525c601492
stefan-spitse <97093546@st.deltion.nl>
2023-05-17 09:45:44 +0200

    Made content for home page

--- content/cps.php
+++ content/cps.php
@@ -1,15 +1,7 @@
 <?php
 function cps()
 {
-    return '
-<div class="container justify-content-center">
-    <div class="p-5">
-       <h1>How fast can you click?</h1>
-    </div>
-    <div class="pl-5">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam architecto eius maiores necessitatibus nisi rem sed velit voluptatum? Aliquam corporis culpa dolore explicabo magnam, optio quam ratione tempora vero!</p>
-    </div>
-</div>';
+    return '';
 }
 
 
--- content/home.php
+++ content/home.php
@@ -2,6 +2,13 @@
 
 function home()
 {
-    $out = "";
-    return "<h1>Home</h1>";
+    return '
+<div class="container justify-content-center">
+    <div class="pl-5 pt-4">
+       <h1>How fast can you click?</h1>
+    </div>
+    <div class="pl-5">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam architecto eius maiores necessitatibus nisi rem sed velit voluptatum? Aliquam corporis culpa dolore explicabo magnam, optio quam ratione tempora vero!</p>
+    </div>
+</div>';
 }
